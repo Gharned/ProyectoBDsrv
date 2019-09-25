68d3a77e1b4896f75cb5ccadb58953914b2031fa
Gharned <gharned.m@gmail.com>
2019-09-24 22:43:46 -0300

    v0.1.3 - Iteracion 1 y 2 Terminada

--- web/src/controllers/contrCustomer.js
+++ web/src/controllers/contrCustomer.js
@@ -1,5 +1,7 @@
 const customer={}
 
+var dataStore; //guarada lo que sera insertado en la renta y se gestionara de pagina en pagina
+
 //pasar datos necesarios
 // 1.-Sucursales para elegir
 customer.infSuc=(req,res)=>{
@@ -22,19 +24,22 @@ customer.infSuc=(req,res)=>{
     });
 };
 
-customer.dispVehiculos=(req,res)=>{
-    //obtengo el id de la sucursal que se encontrará en una region
-    // y buscare los vehiculos que no están rentados de esa sucursal
-    const data=req.body;
+customer.dispVehiculos=(req,res)=>{   //buscare los vehiculos que no están rentados de esa sucursal
+    //obtengo el id de la sucursal que se encontrará en una region de retiro
+    dataStore=req.body;
+    //aqui si no se eligio local de devolucion se deja en el mismo ,  if..
+    dataStore.local_devolucion=dataStore.local_retiro;
+    //console.log(dataStore);
+    
     req.getConnection((err,conn)=>{
-        conn.query('select matricula,tipo,marca,modelo,color,anio,kilometraje,precio from Vehiculo where estado=0 and id_sucursal=?',[data.id_sucursal],(err,vehiculos)=>{
+        conn.query('select matricula,tipo,marca,modelo,color,anio,kilometraje,precio from Vehiculo where estado=0 and id_sucursal=?',[dataStore.local_retiro],(err,vehiculos)=>{
             if(err){
                 console.log('Error con obtencion de vehiculos');
             }
             //envio vehiculos a la pagina redireccionada
             res.render('vCatalogo',{
                 data:vehiculos,
-                dataStore:req.body,
+                dataStore,
             });
         });
     });
@@ -43,50 +48,38 @@ customer.dispVehiculos=(req,res)=>{
 //quizas alla que arreglarlo, es lo unico que se me ocurrio
 customer.paramFilter=(req,res)=>{  //desde aqui debiese tener mi local_r y local_d
     const filtros=req.body; //trae parametros de para el filtro
-    console.log(filtros);
-    
-    var dataStore=new Object(); //rellenado al objeto
-    dataStore.id_sucursal=req.params.id; 
-    dataStore.fecha_retiro=req.params.fecha_r;
-    dataStore.fecha_devolucion=req.params.fecha_d;
-
-    var resultado=""; //guadara consulta concatenada en for
-    for(var i in filtros){
+    //console.log(filtros);
+    var resultado=""; //guadara un string, con los filtros que seran consultados
+    for(var i in filtros){ //for que concatena string de consulta
         if(!(filtros[i]==='')){ //si tiene contenido el filtro lo coloco
-            resultado+=" and "+i+"='"+filtros[i]+"'";
+            resultado+=" and "+i+"='"+filtros[i]+"'"; //tomamos los filtros concatenados para consulta
         }
     }
-    console.log(resultado);
-    
     //console.log(resultado);
     req.getConnection((err,conn)=>{
-        conn.query('select matricula,tipo,marca,modelo,color,anio,kilometraje,precio from Vehiculo where estado=0 and id_sucursal=?'+resultado+';',[dataStore.id_sucursal],(err,fvehiculos)=>{
+        conn.query('select matricula,tipo,marca,modelo,color,anio,kilometraje,precio from Vehiculo where estado=0 and id_sucursal=?'+resultado+';',[dataStore.local_retiro],(err,fvehiculos)=>{
             if(err){
                 console.log('Error con obtencion de vehiculos por filtro');
             }
             res.render('vCatalogo',{
                 data:fvehiculos,
-                dataStore:dataStore,
+                dataStore,
             });
         });
     });
 };
 
-//nota: ya deberia tener la regio, por el seleccionado de vPrincipal, pero no
+//nota: ya deberia tener la region, por el seleccionado de vPrincipal, pero no
 customer.arrendar=(req,res)=>{
-    var dataStore=new Object(); //rellenado al objeto
+    //probando var global
     dataStore.matricula=req.params.matricula;
-    dataStore.local_retiro=req.params.id; //este será mientras tanto el local de retiro y devolucion
-    dataStore.local_devolucion=req.params.id;// lo que dije arriba
-
-    dataStore.fecha_retiro=req.params.fecha_r;
-    dataStore.fecha_devolucion=req.params.fecha_d;
     req.getConnection((err,conn)=>{
         //para conceguir la de "retiro", "devolucion" -->cuando se arregle, hacer 2 querys
         conn.query('select region from Direccion_sucursal where id_sucursal=?;',[dataStore.local_retiro],(err,region_rd)=>{
             if(err){
                 console.log("No se encontro la region");
             }
+            //guardo regiones
             dataStore.region_retiro=region_rd[0].region;
             dataStore.region_devolucion=region_rd[0].region; //deviesen ser 2 querys
             //console.log(dataStore);
@@ -98,7 +91,7 @@ customer.arrendar=(req,res)=>{
                                 console.log('Error con obtencion de datos del vehiculo');
                             }
                             res.render('vArriendo',{
-                                dataSto:dataStore,
+                                dataStore,
                                 dataRetiro:datos_retiro,
                                 dataDev:datos_devolucion,
                                 dataVeh:datos_veh,
@@ -115,18 +108,10 @@ customer.arrendar=(req,res)=>{
 //en otra version ver si dataStore puede funcionar como variable global -> a no ser que influya en todos los customers
 
 customer.finalizar=(req,res)=>{
-    var dataStore=new Object();
-    dataStore.matricula=req.params.matricula;
-    dataStore.local_retiro=req.params.local_retiro; 
-    dataStore.local_devolucion=req.params.local_devolucion;
-    dataStore.fecha_retiro=req.params.fecha_retiro;
-    dataStore.fecha_devolucion=req.params.fecha_devolucion;
-    dataStore.region_retiro=req.params.region_retiro;
-    dataStore.region_devolucion=req.params.region_devolucion;
-    //----------------------------------------------------------
     const cliente=req.body; //trae parametros del formulario cliente
     dataStore.rut_cliente=cliente.rut_cliente;
     dataStore.estado='En Curso';
+
     req.getConnection((err,conn)=>{
         //pregunto si ya existe el cliente la bd
         conn.query('select rut_cliente from Cliente where rut_cliente=?;',[cliente.rut_cliente],(err,rut)=>{
--- web/src/routers/SRVrouter.js
+++ web/src/routers/SRVrouter.js
@@ -10,11 +10,11 @@ router.get("/",contrCustomer.infSuc);
 router.post('/buscar',contrCustomer.dispVehiculos);
 
 //buscar otras maneras, mas adelante
-router.post('/filtrar/:id/:fecha_r/:fecha_d',contrCustomer.paramFilter);
+router.post('/filtrar',contrCustomer.paramFilter);
 
-router.get('/arrendar/:matricula/:id/:fecha_r/:fecha_d',contrCustomer.arrendar);
+router.get('/arrendar/:matricula',contrCustomer.arrendar);
 
-router.post('/finalizada/:matricula/:local_retiro/:fecha_retiro/:region_retiro/:local_devolucion/:fecha_devolucion/:region_devolucion',contrCustomer.finalizar);
+router.post('/finalizada',contrCustomer.finalizar);
 
 module.exports=router; //exportar metodos permitidos para
 //estas rutas
